dabc51843b0a423b0e9fe32b184284292eaf31a3
Anuj Gupta <apgupta3091@gmail.com>
2021-12-03 18:22:25 -0500

    last commit

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -20,6 +20,7 @@ const map = function (){
     pathGenerator = d3.geoPath().projection(usaProjection)
     geoJsonUrl = "https://gist.githubusercontent.com/spiker830/e0d1b7950ced31369c903bed0cead7b1/raw/702c72e0ca5a1be95f84a50a58cfa6d4d6400f3f/us_features.json"
     
+    
     d3.json(geoJsonUrl)
         .then(geojson => {
       svg.selectAll("path")
@@ -30,6 +31,7 @@ const map = function (){
         .attr("stroke", "black") 
         .attr("fill", "white") 
         .attr("class", "state")
+        
         });
 }
 
