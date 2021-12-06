b226afcdaf222eaa7dbe1f493491d64bfdb749cb
Anuj Gupta <apgupta3091@gmail.com>
2021-12-06 15:29:02 -0500

    Set up Server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import legend from './scripts/legend';
 import map from './scripts/map';
-import data from './scripts/data';
 
 document.addEventListener("DOMContentLoaded", () => {
     map();
     legend();
-    data();
+    
 })
+
+const demoUrl = 'https://www.metaweather.com/api/location/search/?query=san';
+    const corsRequest = (url = demoUrl) => {
+  fetch(`/cors?url=${encodeURIComponent(url)}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+}
+
+    const apiRequest = (query = 'curry') => {
+  fetch(`/api?searchTerm=${encodeURIComponent(query)}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+}
